1f57265665b6fe8e6112a0ea529c08f95b24a159
Darko Pancev <igededarkopancev@gmail.com>
2025-03-10 11:20:45 +0800

    Add initial velite integration

---
+++ next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  devIndicators: {
+    appIsrStatus: false,
+  },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: "**",
+      },
+    ],
+  },
+  webpack: config => {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
+  }
+}
+
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      const { build } = await import('velite')
+      await build({ watch: dev, clean: !dev })
+    })
+  }
+}
---
+++ velite.config.ts
@@ -0,0 +1,118 @@
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeSlug from "rehype-slug"
+import { defineCollection, defineConfig, s } from "velite"
+import { codeImport } from "remark-code-import"
+import remarkGfm from "remark-gfm"
+import { visit } from "unist-util-visit"
+
+import { rehypeComponent } from "@/lib/rehype-component"
+import { rehypeNpmCommand } from "@/lib/rehype-npm-command"
+
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+})
+
+const docs = defineCollection({
+  name: "Doc",
+  pattern: "docs/**/*.mdx",
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      description: s.string().max(999).optional(),
+      published: s.boolean().default(true),
+      links: s.string().array().optional(),
+      toc: s.toc(),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
+})
+
+export default defineConfig({
+  root: "content",
+  output: {
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true,
+  },
+  collections: { docs },
+  mdx: {
+    remarkPlugins: [remarkGfm, codeImport],
+    rehypePlugins: [
+      rehypeSlug as any,
+      rehypeComponent,
+      () => (tree) => {
+        visit(tree, (node) => {
+          if (node?.type === "element" && node?.tagName === "pre") {
+            const [codeEl] = node.children
+            if (codeEl.tagName !== "code") {
+              return
+            }
+
+            if (codeEl.data?.meta) {
+              // Extract event from meta and pass it down the tree.
+              const regex = /event="([^"]*)"/
+              const match = codeEl.data?.meta.match(regex)
+              if (match) {
+                node.__event__ = match ? match[1] : null
+                codeEl.data.meta = codeEl.data.meta.replace(regex, "")
+              }
+            }
+
+            node.__rawString__ = codeEl.children?.[0].value
+            node.__src__ = node.properties?.__src__
+            node.__style__ = node.properties?.__style__
+          }
+        })
+      },
+      [
+        rehypePrettyCode,
+        { theme: "github-dark-dimmed", keepBackground: false, grid: false },
+      ],
+      () => (tree) => {
+        visit(tree, (node) => {
+          if (node?.type === "element" && node?.tagName === "figure") {
+            if (!("data-rehype-pretty-code-figure" in node.properties)) {
+              return
+            }
+
+            const preElement = node.children.at(-1)
+            if (preElement.tagName !== "pre") {
+              return
+            }
+
+            preElement.properties["__withMeta__"] =
+              node.children.at(0).tagName === "div"
+            preElement.properties["__rawString__"] = node.__rawString__
+
+            if (node.__src__) {
+              preElement.properties["__src__"] = node.__src__
+            }
+
+            if (node.__event__) {
+              preElement.properties["__event__"] = node.__event__
+            }
+
+            if (node.__style__) {
+              preElement.properties["__style__"] = node.__style__
+            }
+          }
+        })
+      },
+      rehypeNpmCommand,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
+  },
+})
